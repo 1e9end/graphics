812f7e79a3644cccf8af8d7804f4cbacc05b367c
polarr <charles.ran9@gmail.com>
2024-05-11 12:42:49 -0400

    feat: rendering parameters controller, updated gui

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var gl;
 gl = initGL("canvas");
-var program = createProgram("vts", "fts");
+
+let program = createProgram("vts", "fts");
 gl.useProgram(program);
 
-var posL = gl.getAttribLocation(program, "position");
-var posBuffer = gl.createBuffer();
+let posL = gl.getAttribLocation(program, "position");
+let posBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
 
-var positions = [
+let positions = [
     -1, -1, 
     +1, -1,
     -1, +1, 
@@ -19,23 +19,31 @@ var positions = [
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
 resize(gl.canvas, false, false, true);
-document.getElementById("ui").style.width = window.innerWidth - window.innerHeight + "px";
 clearColor();
 
 gl.enableVertexAttribArray(posL);
 gl.vertexAttribPointer(posL, 2, gl.FLOAT, false, 0, 0);
 
-var resL = gl.getUniformLocation(program, "res");
+let config = {
+    fractal: 3,
+    specular_exp: 16,
+    light_angle: 0.0,
+    fps: 60,
+};
+
+let resL = gl.getUniformLocation(program, "res");
 gl.uniform2f(resL, 2.0/gl.drawingBufferWidth, 2.0/gl.drawingBufferHeight);
-var timeL = gl.getUniformLocation(program, "time");
-var fractalL = gl.getUniformLocation(program, "fractal");
-var camZL = gl.getUniformLocation(program, "camZ");
-var THICCL = gl.getUniformLocation(program, "THICC");
+let timeL = gl.getUniformLocation(program, "time");
+let fractalL = gl.getUniformLocation(program, "fractal");
+let camZL = gl.getUniformLocation(program, "camZ");
+let THICCL = gl.getUniformLocation(program, "THICC");
+let specularExpL = gl.getUniformLocation(program, "specularExp");
+let lightAngleL = gl.getUniformLocation(program, "lightAngle");
 
 function updateFractal(){
-    var fractalN = document.getElementById("fractals").value;
-    var camZ, THICC;
-    switch(+fractalN){
+    // let fractalN = document.getElementById("fractals").value;
+    let camZ, THICC;
+    switch(+config.fractal){
         case 3:
             camZ = -3.0;
             THICC = 0.001;
@@ -52,26 +60,84 @@ function updateFractal(){
             camZ = -6.0;
             THICC = 0.001;
     }
-    gl.uniform1i(fractalL, fractalN);
+    gl.uniform1i(fractalL, config.fractal);
     gl.uniform1f(camZL, camZ);
     gl.uniform1f(THICCL, THICC);
+    gl.uniform1f(specularExpL, config.specular_exp);
+    gl.uniform1f(lightAngleL, config.light_angle);
 }
+
 updateFractal();
+
+let gui = new dat.GUI({ width: 300 });
+
+gui.add(config, 'fractal', {
+    'Menger Sponge': 3,
+    'Sierpenski Pyramid': 0,
+    'Mandelbox': 1,
+    'Mandelbulb': 2,
+}).name('Fractal').onFinishChange(updateFractal);
+
+gui.add(config, 'specular_exp', 0.5, 16, 0.5).name('Specular Exponent').onFinishChange(updateFractal);
+
+gui.add(config, 'light_angle', 0.0, 1, 0.1).name('Lighting Angle').onFinishChange(updateFractal);
+
+//let perfFolder = gui.addFolder("Performance");
+//perfFolder.add(config, 'fps', 0, 120).name('Target FPS').step(1);
 
 // Choose best requestAnimationFrame
 window.reqAnimationFrame = (function(callback){
     return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
     function(callback) {
-      window.setTimeout(callback, 1000 / 60);
+      window.setTimeout(callback, 1000 / config.fps);
     };
 })();
-var startTime = (new Date()).getTime();
+
+let stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
+
+let startTime = performance.now();
+
 function draw(){
-    var time = (new Date()).getTime();
+    let time = performance.now();
+
+    stats.begin();
 
     gl.drawArrays(gl.TRIANGLES, 0, 6);
     gl.uniform1f(timeL, (time - startTime)/2000);
-    
-    window.reqAnimationFrame(draw, 1000/60);
+
+    stats.end();
+
+    window.reqAnimationFrame(draw, 1000/ config.fps);
 }
+
+/**
+function animate(newtime) {
+    // request another frame
+    window.reqAnimationFrame(animate);
+
+    // elapsed time since last loop
+    now = newtime;
+    elapsed = now - then;
+
+    // if enough time has elapsed, draw the next frame
+
+    if (elapsed > fpsInterval) {
+
+        // Get ready for next frame by setting then=now, but...
+        // Also, adjust for fpsInterval not being multiple of 16.67
+        then = now - (elapsed % fpsInterval);
+
+        // draw stuff here
+
+
+        // TESTING...Report #seconds since start and achieved fps.
+        var sinceStart = now - startTime;
+        var currentFps = Math.round(1000 / (sinceStart / ++frameCount) * 100) / 100;
+        $results.text("Elapsed time= " + Math.round(sinceStart / 1000 * 100) / 100 + " secs @ " + currentFps + " fps.");
+
+    }
+}**/
+
 window.addEventListener("load", draw, false);
